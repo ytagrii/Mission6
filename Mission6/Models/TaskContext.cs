9b6d9bba9e5f5b01de6452415750fab1f858a750
mperry5th <90285075+mperry5th@users.noreply.github.com>
2022-02-07 11:23:59 -0700

    All model files created, context file done

---
+++ Mission6/Models/TaskContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission6.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> task { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Quadrant> quadrant{ get; set; }
+    }
+}
